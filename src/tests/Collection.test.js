77a7e7f6ada325bf706423ce668205f46c29fdb9
aliNzLami <ali.nabizadeh79@yahoo.com>
2025-09-05 05:24:27 +0330

    Refactor test: collection page components

--- src/tests/Collection.test.js
+++ src/tests/Collection.test.js
@@ -1,9 +1,54 @@
-// import { render, screen } from '@testing-library/react';
-// import Collection from '../pages/Collection/Collection';
+import { render, screen, waitFor } from '@testing-library/react';
 
-// test('renders Contact Us heading', () => {
-//   render(<Collection />);
-//   const heading = screen.queryByText(/COLLECTIONS/i );
-//   expect(heading).toBeInTheDocument();
-// });
+// component
+import Collection from '../pages/Collection/Collection';
 
+// context
+import ShopContextProvider from '../assets/context/ShopContext.jsx';
+
+// route
+import { MemoryRouter } from 'react-router';
+
+
+
+
+const renderPage = () => {
+    return render(
+        <MemoryRouter>
+            <ShopContextProvider>
+                <Collection />
+            </ShopContextProvider>
+        </MemoryRouter>
+    );
+}
+
+
+test('renders Contact Us heading', async () => {
+    renderPage()
+
+    await waitFor(() => {
+        const heading = screen.queryByText(/COLLECTIONS/i );
+        expect(heading).toBeInTheDocument();
+    })
+});
+
+test('renders products: products aray length must not be zero', async () => {
+    const { container } = renderPage();
+    
+    await waitFor(() => {
+        const products = [...container.getElementsByClassName('productCard')];
+        expect(products).not.toHaveLength(0);
+    })
+});
+
+test('filters exist', async () => {
+    const { container } = renderPage()
+
+    await waitFor(() => {
+        const heading = screen.queryByText(/filter/i );
+        expect(heading).toBeInTheDocument();
+
+        const filtersList = [...container.getElementsByClassName('filterbox_collection')];
+        expect(filtersList).not.toHaveLength(0);
+    })
+});
